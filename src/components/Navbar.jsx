aa01a5188acdfcb20b4983cef1a7ca0f3ef2243e
Noah_Dave <m.davenoa@gmail.com>
2025-03-22 22:15:56 +0300

    resume button updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
     <nav className="fixed top-0 w-full z-40 bg-[rgba(35, 159, 109, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="#about" className="btn-shine font-mono text-xl font-bold text-white">
+          <a href="#about" className="btn-shine font-mono text-lg sm:text-2xl md:text-3xl lg:text-4xl font-bold text-white">
             {" "}
-            noa<span className="text-blue-500">.dev</span>{" "}
+            <span className="text-green-500 text-3xl">noa.dave</span>{" "}
           </a>
 
           <div
--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -54,16 +54,28 @@ export const About = () => {
           </p>
 
           <div className="flex justify-center space-x-8">
-            <a
-              href="https://docs.google.com/document/d/1g9Jg6m9U9FqEujTOjPG6xzKaenJ5mJ8SaVFmxm29EwA/edit?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="button type1">
-                <span>.</span>
-                
-              </button>
-            </a>
+          <a
+  href="https://docs.google.com/document/d/1g9Jg6m9U9FqEujTOjPG6xzKaenJ5mJ8SaVFmxm29EwA/edit?usp=sharing"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button className="button" type="button">
+    <span className="button__text">RESUME</span>
+    <span className="button__icon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 35 35"
+        id="bdd05811-e15d-428c-bb53-8661459f9307"
+        data-name="Layer 2"
+        className="svg"
+      >
+        <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+        <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+        <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+      </svg>
+    </span>
+  </button>
+</a>
             <a
                href="#contact" 
               className="relative group inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
