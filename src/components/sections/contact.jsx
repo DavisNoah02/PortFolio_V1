3f75ce900ec290f481cbae3770b53bcae79e0ffa
Noah_Dave <m.davenoa@gmail.com>
2025-03-23 23:58:13 +0300

    contact emailjs

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -21,7 +21,7 @@ export const Contact = () => {
         e.target,
         import.meta.env.VITE_PUBLIC_KEY
       )
-      .then((result) => {
+      .then(() => {
         alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
       })
@@ -113,7 +113,7 @@ export const Contact = () => {
 
               <button
                 type="submit"
-                className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+                className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,256,0.4)]"
               >
                 Send Message
               </button>
