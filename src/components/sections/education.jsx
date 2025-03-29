133d83fb6d330a79a3aeb2628fc72f1a25445548
Noah_Dave <m.davenoa@gmail.com>
2025-03-29 06:12:41 +0300

    edu pics file path

--- src/components/sections/education.jsx
+++ src/components/sections/education.jsx
@@ -9,7 +9,7 @@ export const Education = () => {
       institution: "Chuka University",
       description: "Specializing in Artificial Intelligence and Machine Learning.",
       grade: "2nd Class Honors Upper Division",
-      iconSrc: "/public/images/Chuka.jpeg", 
+      iconSrc: "/Chuka.jpeg", 
     },
     {
       year: "2021 May - 2021 Aug",
@@ -17,7 +17,7 @@ export const Education = () => {
       institution: "Strathmore University",
       description: "Focused on software development and data structures.",
       grade: "PASS",
-      iconSrc: "/public/images/Strathmore.png", 
+      iconSrc: "/Strathmore.png", 
     },
   ];
 
